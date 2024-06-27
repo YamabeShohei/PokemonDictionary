4661a9823313dc6cf0cf01524b449df31aa520d4
yamabe.shohei <mfo.obr314314@gmail.com>
2024-06-27 22:03:15 +0900

    APIをfetchする処理を実装

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import "./App.css"
+import {getAllPokemon} from "./utils/pokemon"
+
+function App(){
+    const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            const res = await getAllPokemon(initialURL);
+            console.log(res);
+        }
+
+        fetchPokemonData();
+    }, [])
+
+    return <div className="App"></div>
+}
+
+export default App;
---
+++ src/utils/pokemon.js
@@ -0,0 +1,7 @@
+export const getAllPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => resolve(data));
+    });
+}
