7525d157798aa49d5d69a5b33caadfafb10a26ad
yamabe.shohei <mfo.obr314314@gmail.com>
2024-06-27 22:22:03 +0900

    詳細データの取得処理を実装

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css"
-import {getAllPokemon} from "./utils/pokemon"
+import {getAllPokemon, getPokemon} from "./utils/pokemon"
 
 function App(){
     const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+    const [loading, setLoading] = useState(true);
+    const [pokemonData, setPokemonData] = useState([]);
 
     useEffect(() => {
         const fetchPokemonData = async () => {
             const res = await getAllPokemon(initialURL);
-            console.log(res);
+            loadPokemon(res.results);
+            setLoading(false);
         }
 
         fetchPokemonData();
     }, [])
 
-    return <div className="App"></div>
+    const loadPokemon = async (data) => {
+        const pokemonData = await Promise.all(data.map((pokemon) => {
+            return getPokemon(pokemon.url);
+        }));
+        setPokemonData(pokemonData);
+    }
+
+    return <div className="App">
+        {loading ? (
+            <h1>ロード中</h1>
+        ): (
+            <>
+            <h1>データを取得しました</h1>
+            </>
+        )}
+    </div>
 }
 
 export default App;
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -5,3 +5,11 @@ export const getAllPokemon = (url) => {
             .then(data => resolve(data));
     });
 }
+
+export const getPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => resolve(data));
+    });
+}
